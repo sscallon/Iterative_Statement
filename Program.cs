76774e8ea3aac3c3a1ca5c5de93ca9f513783cb5
Shane Scallon <sscallon@usf.edu>
2020-01-29 18:43:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//Programmer: Shane Scallon
+//Date: 1/29/2020
+//Description: This C# console application demonstrates iterative statments
+//after getting input from users
+
+using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking the user for an input of an interger
+            Console.WriteLine("Enter an interger between 1 and 100");
+
+            //Try-catch to validate user input
+            //when bad input is used a message will show stating an interger should be entered
+            try
+            {
+                //Variable gathers input from users
+                string input = Console.ReadLine();
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Please run the program again and enter an interger value");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+            }
+
+        }
+    }
+}
